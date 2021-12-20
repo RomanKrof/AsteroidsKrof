655fef366c75e15991e50f4b4867fc0931fbb9a2
Roman Krof <krof.roman.2018@skola.ssps.cz>
2021-12-20 21:06:20 +0100

    Update

---
+++ AsteroidsKrof/APICaller.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AsteroidsKrof
+{
+    class APICaller
+    {
+        public static async Task<API_Data>
+            Get(string li)
+        {
+            using (var http = new HttpClient())
+            {
+                var req = await http.GetAsync(li);
+
+                if (req.IsSuccessStatusCode)
+                {
+                    return new API_Data { Data = await req.Content.ReadAsStringAsync() };
+                }
+
+                else 
+                {
+                    return null;
+                }                    
+            }
+        }
+
+    }
+    public class API_Data
+    {
+        public string Data { get; set; }
+    }
+}
--- AsteroidsKrof/MainWindow.xaml.cs
+++ AsteroidsKrof/MainWindow.xaml.cs
@@ -12,7 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
 
 namespace AsteroidsKrof
 {
@@ -21,15 +25,43 @@ namespace AsteroidsKrof
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Omlouvám se, že jsem projekt nedokončil, bohužel jsem po usilovném zkoušení nedokázal přijít na to, jak implementovat API do mého programu.
+        static RootObject AsteroidData;
+
+        static API_Data info;
+
+        public static ObservableCollection<Asteroid> AsteroidsO { get; set; }
+
+        //Omlouvám se, že program není funkční. Spolupracoval jsem s kolegou Mihalovičem, ale i přesto se nám nepodařilo program zprovoznit.
+
         public MainWindow()
         {
             InitializeComponent();
-            string API_Key = "fJs7EBzFWHxyYAN4PA8CMaAEWcYRzbTVyqfsqf1z";
-            DateTime n = DateTime.Now;
-            string link = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={n.Year}-{n.Month}-{n.Day}&end_date={n.Year}-{n.Month}-{n.Day}&api_key={API_Key}";          
+            GetData();
         }
 
+        private async void GetData()
+        {
+            string API_KEY = "fJs7EBzFWHxyYAN4PA8CMaAEWcYRzbTVyqfsqf1z";
+            DateTime n = DateTime.Now;
+            string odkaz = $"https://api.nasa.gov/neo/rest/v1/feed?start_date={n.Year}-{n.Month}-{n.Day}&end_date={n.Year}-{n.Month}-{n.Day}&api_key={API_KEY}";
+
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)
+            {
+
+                info = await APICaller.Get(odkaz);
+
+                if (info.Data != null && info.Data != "")
+                {
+                    Regex reg = new Regex(@"\d{4}[-]\d{2}[-]\d{2}");
+                    string database = reg.Replace(info.Data, "date", 1, 500);
+
+                    AsteroidData = JsonConvert.DeserializeObject<RootObject>(database);
+                    AsteroidsO = new ObservableCollection<Asteroid>(AsteroidData.near_earth_objects.asteroids);
+                }
+
+            }
+
+        }
         private void Asteroid_List_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
@@ -37,7 +69,8 @@ namespace AsteroidsKrof
 
         private void Asteroid_List_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            InfoBox ib = new InfoBox();
+            ib.ShowDialog();
         }
-    }
+    }       
 }
